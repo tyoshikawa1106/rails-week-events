ed7a99083afeae21a4a2ae833936018c57b4bf99
Taiki <tyoshikawa1106@gmail.com>
2015-10-02 02:01:04 +0900

    commit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,17 @@
 class StaticPagesController < ApplicationController
   def index
-    @events_all = Event.all();
+    @events_all = Event.all()
+    @events_sun = []
+    @events_mon = []
+    @events_tue = []
+    @events_wed = []
+    @events_thu = []
+    @events_fri = []
+    @events_sat = []
+
+    for event in @events_all do
+      @events_sun.push(event)
+    end
+
   end
 end
