7658af66e6f99b4f85b7d481c2deab913db4e20a
Taiki <tyoshikawa1106@gmail.com>
2015-10-02 13:05:12 +0900

    commit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,39 +1,14 @@
 class StaticPagesController < ApplicationController
+  include StaticPagesHelper
+
   def index
-    @events_all = Event.all().where.not(start_date_time: nil).order('start_date_time asc')
-    @events_sun = []
-    @events_mon = []
-    @events_tue = []
-    @events_wed = []
-    @events_thu = []
-    @events_fri = []
-    @events_sat = []
-
-    # 曜日毎のEventsを取得
-    getWeekEvents();
+    # クラス変数の初期化
+    init()
+    # すべてのEvent情報を取得
+    @events_all = getAllEvents()
+    # 今週のEvent情報を取得
+    wk_events = getWeekEvents()
+    # 曜日毎のEventsに値をセット
+    setWeekEvents(wk_events);
   end
-
-  private
-    # 曜日毎のEventsを取得
-    def getWeekEvents
-      for event in @events_all do
-        wday = event.start_date_time.wday
-
-        if wday === 0
-          @events_sun.push(event)
-        elsif wday === 1
-          @events_mon.push(event)
-        elsif wday === 2
-          @events_tue.push(event)
-        elsif wday === 3
-          @events_wed.push(event)
-        elsif wday === 4
-          @events_thu.push(event)
-        elsif wday === 5
-          @events_fri.push(event)
-        elsif wday === 6
-          @events_sat.push(event)
-        end
-      end
-    end
 end
--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,53 @@
 module StaticPagesHelper
+
+  # クラス変数の初期化
+  def init
+    @events_all = []
+    @events_sun = []
+    @events_mon = []
+    @events_tue = []
+    @events_wed = []
+    @events_thu = []
+    @events_fri = []
+    @events_sat = []
+  end
+
+  # 全てのEvent情報を取得
+  def getAllEvents
+    return Event.all()
+                .where.not(start_date_time: nil)
+                .order('start_date_time desc')
+                .limit(200)
+  end
+
+  # 今週のEvent情報を取得
+  def getWeekEvents
+    return Event.all()
+                .where.not(start_date_time: nil)
+                .order('start_date_time asc')
+                .limit(200)
+  end
+
+  # 曜日毎のEventsに値をセット
+  def setWeekEvents(wk_events)
+    for event in wk_events do
+      wday = event.start_date_time.wday
+
+      if wday === 0
+        @events_sun.push(event)
+      elsif wday === 1
+        @events_mon.push(event)
+      elsif wday === 2
+        @events_tue.push(event)
+      elsif wday === 3
+        @events_wed.push(event)
+      elsif wday === 4
+        @events_thu.push(event)
+      elsif wday === 5
+        @events_fri.push(event)
+      elsif wday === 6
+        @events_sat.push(event)
+      end
+    end
+  end
 end
